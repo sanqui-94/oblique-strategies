f0fac9b315b01f04094ae62f8ff2613e120ff18d
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2024-09-05 22:00:34 -0400

    Adding material UI

---
+++ src/components/Content.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Content() {
+  return (
+    <section>
+      <Typography variant="h4" sx={{ textTransform: "uppercase" }}>
+        Oblique Strategies
+      </Typography>
+    </section>
+  );
+}
